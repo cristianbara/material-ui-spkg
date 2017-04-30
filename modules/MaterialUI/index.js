d00c193b42c30f4d28773245c9aabeb8496fd477
Alexander Pustovalov <apustovalov@gmail.com>
2017-04-30 11:07:35 +0300

    Add new components and test pages

--- modules/MaterialUI/index.js
+++ modules/MaterialUI/index.js
@@ -2,26 +2,26 @@ require('./css/styles.css');
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import ThemeProvider from './components/ThemeProvider';
 import {
-    List,
-    ListItem
+  List,
+  ListItem
 } from 'material-ui/List';
 import Paper from 'material-ui/Paper';
 import FlatButton from 'material-ui/FlatButton';
-import ListItemWithLeftIcon from './components/ListItemWithLeftIcon';
-import ListItemWithRightIcon from './components/ListItemWithRightIcon';
 import RaisedButton from 'material-ui/RaisedButton';
-import RaisedButtonWithIcon from './components/RaisedButtonWithIcon';
+import AppBar from 'material-ui/AppBar';
+import AutoComplete from 'material-ui/AutoComplete';
+import Avatar from 'material-ui/Avatar';
 if (window.alreadyInjected) {
-    injectTapEventPlugin();
+  injectTapEventPlugin();
 }
 export {
-    ThemeProvider,
-    FlatButton,
-    List,
-    ListItem,
-    ListItemWithLeftIcon,
-    ListItemWithRightIcon,
-    RaisedButton,
-    Paper,
-    RaisedButtonWithIcon
+  ThemeProvider,
+  FlatButton,
+  List,
+  ListItem,
+  RaisedButton,
+  Paper,
+  AppBar,
+  AutoComplete,
+  Avatar
 };
